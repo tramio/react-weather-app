7a4c56168cf17ad9653daf4ec76973d21ccaa0b7
Tramio <tramiodev@outlook.fr>
2022-04-20 19:19:41 +0200

    transpose error handling from original vanilla.js project to this react.js project

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,12 +20,46 @@ class Main extends Component {
   meterPerSecondToKilometerPerHour = (mps) => {
     return mps * 3.495;
   }
+  hideForecastCard = () => {
+    const forecastCard = document.querySelector(".forecast-card");
+    forecastCard.style.display = "none";
+  }
+  showForecastCard = () => {
+    const forecastCard = document.querySelector(".forecast-card");
+    forecastCard.style.display = "block";
+  }
+  displayErrorCard = () => {
+    const h1 = document.createElement("h1");
+    h1.textContent = "Uh-oh";
+
+    const p = document.createElement("p");
+    const locationInput = this.getLocationInput();
+    p.textContent = `We couldn't find a location named "${locationInput}". Please check your spelling and try again. 🌞`;
+    
+    const errorCard = document.createElement("div");
+    errorCard.setAttribute("id", "error-message");
+    errorCard.classList.add("card", "error-card");
+    errorCard.appendChild(h1);
+    errorCard.appendChild(p);
+
+    const main = document.querySelector("main");
+    main.appendChild(errorCard);
+  }
+  hideErrorCard = () => {
+    const errorMessage = document.getElementById("error-message");
+    const main = document.querySelector("main");
+    if (errorMessage) {
+      main.removeChild(errorMessage);
+    }
+  }
   fetchForecast(location) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0982e2845899ef11e0c7aebc11449571&units=metric`, {mode: 'cors'})
       .then((response) => {
         return response.json();
       })
       .then((response) => {
+        this.hideErrorCard();
+        this.showForecastCard();
         this.setState({
           location: response.name,
           main: response.weather[0].main,
@@ -34,6 +68,10 @@ class Main extends Component {
           wind: parseInt(this.meterPerSecondToKilometerPerHour(response.wind.speed)),
           humidity: response.main.humidity,
         });
+      })
+      .catch((error) => {
+        this.hideForecastCard();
+        this.displayErrorCard();
       });
   }
   componentDidMount() {
@@ -52,25 +90,25 @@ class Main extends Component {
           <input id="location-input" type="text" placeholder="ex: Jieyang"></input>
           <button id="location-submit" type="button" onClick={this.onLocationSubmit}>Let's go</button>
         </div>
-        <div className="card">
-          <h1 id="location" className="forecast">{this.state.location}</h1>
-          <p id="temperature" className="forecast">{this.state.temperature} °C</p>
-          <p id="main-forecast" className="forecast">{this.state.main}</p>
+        <div className="card forecast-card">
+          <h1 id="location">{this.state.location}</h1>
+          <p id="temperature">{this.state.temperature} °C</p>
+          <p id="main-forecast">{this.state.main}</p>
           <ul>
             <li>
               <h2>Humidity</h2>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-droplet"><path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z"></path></svg>
-              <p id="humidity" className="forecast">{this.state.humidity} %</p>
+              <p id="humidity">{this.state.humidity} %</p>
             </li>
             <li>
               <h2>Feels like</h2>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thermometer"><path d="M14 14.76V3.5a2.5 2.5 0 0 0-5 0v11.26a4.5 4.5 0 1 0 5 0z"></path></svg>
-              <p id="feels-like" className="forecast">{this.state.feelsLike} °C</p>
+              <p id="feels-like">{this.state.feelsLike} °C</p>
             </li>
             <li>
               <h2>Wind speed</h2>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-wind"><path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2"></path></svg>
-              <p id="wind" className="forecast">{this.state.wind} km/h</p>
+              <p id="wind">{this.state.wind} km/h</p>
             </li>
           </ul>
         </div>
